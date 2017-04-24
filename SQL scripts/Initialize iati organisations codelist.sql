8804d47fc678269533c90353228c2b3970cd0819
Herman van Loon <herman-van.loon@minbuza.nl>
2017-04-24 16:24:54 +0200

    Initialise DB table for extended org name handling

---
+++ SQL scripts/Initialize iati organisations codelist.sql
@@ -0,0 +1,26 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.10-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table iatireference.codelist-iati-organisations
+DROP TABLE IF EXISTS `codelist-iati-organisations`;
+CREATE TABLE IF NOT EXISTS `codelist-iati-organisations` (
+  `org-iati-id` char(100) COLLATE utf8mb4_bin NOT NULL,
+  `org-title` varchar(256) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`org-iati-id`),
+  KEY `idx_codelist-iati-organisations_lookup` (`org-iati-id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
